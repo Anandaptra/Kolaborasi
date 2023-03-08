5382614eabd7fad7ad1d737889f7e94d174347a2
ASUS-PC <prcllputri1752@gmail.com>
2023-03-08 21:47:19 +0700

    Latihan pricill

--- src/main/kotlin/Pricill.kt
+++ src/main/kotlin/Pricill.kt
@@ -1,3 +1,26 @@
 fun main(){
-    println("Hai, aku pricil")
+   // println("Hai, aku pricil")
+    print("Masukkan angka pertama: ")
+    val angka1 = readLine()!!.toDouble()
+
+    print("Masukkan angka kedua: ")
+    val angka2 = readLine()!!.toDouble()
+
+    print("Masukkan operator (+, -, *, /): ")
+    val operasi = readLine()
+
+    val hasil: Double?
+
+    when (operasi) {
+        "+" -> hasil = angka1 + angka2
+        "-" -> hasil = angka1 - angka2
+        "*" -> hasil = angka1 * angka2
+        "/" -> hasil = angka1 / angka2
+        else -> {
+            println("Operator yang dimasukkan tidak valid")
+            return
+        }
+    }
+
+    println("Hasil dari $angka1 $operasi $angka2 adalah $hasil")
 }
