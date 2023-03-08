afa03bd86a94d55b4a24e09c35b0f6518c965cae
BAGAS <anndptra658@gmail.com>
2023-03-08 21:51:22 +0700

    Nandaa

--- src/main/kotlin/Nanda.kt
+++ src/main/kotlin/Nanda.kt
@@ -1,3 +1,20 @@
 fun main() {
-    var nama: String = "NAnda"
+    print("Masukkan angka pertama: ")
+    val angka1 = readLine()!!.toInt()
+
+    print("Masukkan angka kedua: ")
+    val angka2 = readLine()!!.toInt()
+
+    print("Operasi (+, -, *, /): ")
+    val operator = readLine()
+
+    val hasil = when (operator) {
+        "+" -> angka1 + angka2
+        "_" -> angka1 - angka2
+        "*" -> angka1 * angka2
+        "/" -> angka1 / angka2
+        else -> throw IllegalArgumentException("tidak valid")
+    }
+
+    println("Hasil : $angka1 $operator $angka2 = $hasil")
 }
